2a654fca71dca882ed31d2d475f660ff1b1b7c4a
Moritz Bunkus <moritz@bunkus.org>
2010-06-04 08:46:17 +0000

    Set version number to 1.0.0
    git-svn-id: https://matroska.svn.sourceforge.net/svnroot/matroska/trunk/libmatroska@293 a6f86f6d-0131-4f8e-9e7b-e335508773d5

--- matroska/KaxVersion.h
+++ matroska/KaxVersion.h
@@ -9,12 +9,12 @@
 ** modify it under the terms of the GNU Lesser General Public
 ** License as published by the Free Software Foundation; either
 ** version 2.1 of the License, or (at your option) any later version.
-** 
+**
 ** This library is distributed in the hope that it will be useful,
 ** but WITHOUT ANY WARRANTY; without even the implied warranty of
 ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 ** Lesser General Public License for more details.
-** 
+**
 ** You should have received a copy of the GNU Lesser General Public
 ** License along with this library; if not, write to the Free Software
 ** Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
@@ -40,9 +40,9 @@
 
 START_LIBMATROSKA_NAMESPACE
 
-#define LIBMATROSKA_VERSION 0x000900
+#define LIBMATROSKA_VERSION 0x010000
 
-static const std::string KaxCodeVersion = "0.9.0";
+static const std::string KaxCodeVersion = "1.0.0";
 static const std::string KaxCodeDate    = __TIMESTAMP__;
 
 /*!

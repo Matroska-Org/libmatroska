c5e1f64a435507f0918e970300a1a536c4d3ed35
Moritz Bunkus <moritz@bunkus.org>
2011-09-11 09:04:00 +0000

    Move definition of KaxCodeVersion into the library
    git-svn-id: https://matroska.svn.sourceforge.net/svnroot/matroska/trunk/libmatroska@804 a6f86f6d-0131-4f8e-9e7b-e335508773d5

--- matroska/KaxVersion.h
+++ matroska/KaxVersion.h
@@ -42,8 +42,8 @@ START_LIBMATROSKA_NAMESPACE
 
 #define LIBMATROSKA_VERSION 0x010200
 
-static const std::string KaxCodeVersion = "1.2.0";
-static const std::string KaxCodeDate    = __TIMESTAMP__;
+extern const std::string KaxCodeVersion;
+extern const std::string KaxCodeDate;
 
 /*!
 	\todo Improve the CRC/ECC system (backward and forward possible ?) to fit streaming/live writing/simple reading
--- src/KaxVersion.cpp
+++ src/KaxVersion.cpp
@@ -11,12 +11,12 @@
 ** modify it under the terms of the GNU Lesser General Public
 ** License as published by the Free Software Foundation; either
 ** version 2.1 of the License, or (at your option) any later version.
-** 
+**
 ** This library is distributed in the hope that it will be useful,
 ** but WITHOUT ANY WARRANTY; without even the implied warranty of
 ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 ** Lesser General Public License for more details.
-** 
+**
 ** You should have received a copy of the GNU Lesser General Public
 ** License along with this library; if not, write to the Free Software
 ** Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
@@ -36,5 +36,8 @@
 #include "matroska/KaxVersion.h"
 
 START_LIBMATROSKA_NAMESPACE
+
+const std::string KaxCodeVersion = "1.2.0";
+const std::string KaxCodeDate    = __TIMESTAMP__;
 
 END_LIBMATROSKA_NAMESPACE

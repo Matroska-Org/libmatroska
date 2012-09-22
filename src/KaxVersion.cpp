3fbe2877fbe3f8d01ccf925fd8c4a6fcfbb6753b
Moritz Bunkus <moritz@bunkus.org>
2012-09-22 22:30:44 +0000

    Bump version number, library .so version number, add ChangeLog info
    git-svn-id: https://matroska.svn.sourceforge.net/svnroot/matroska/trunk/libmatroska@849 a6f86f6d-0131-4f8e-9e7b-e335508773d5

--- src/KaxVersion.cpp
+++ src/KaxVersion.cpp
@@ -37,7 +37,7 @@
 
 START_LIBMATROSKA_NAMESPACE
 
-const std::string KaxCodeVersion = "1.3.0";
+const std::string KaxCodeVersion = "1.4.0";
 const std::string KaxCodeDate    = __TIMESTAMP__;
 
 END_LIBMATROSKA_NAMESPACE

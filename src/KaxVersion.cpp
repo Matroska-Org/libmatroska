4934049696ef0111251eb72cc6645f77d1d13246
Moritz Bunkus <moritz@bunkus.org>
2015-10-17 16:02:54 +0200

    Bump version number, add release message

--- src/KaxVersion.cpp
+++ src/KaxVersion.cpp
@@ -37,7 +37,7 @@
 
 START_LIBMATROSKA_NAMESPACE
 
-const std::string KaxCodeVersion = "1.4.2";
+const std::string KaxCodeVersion = "1.4.3";
 const std::string KaxCodeDate    = __TIMESTAMP__;
 
 END_LIBMATROSKA_NAMESPACE

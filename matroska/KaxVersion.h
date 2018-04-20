2cf0198cf3df9ced38c27fac9d45bea4fe8fd74f
Moritz Bunkus <moritz@bunkus.org>
2018-04-20 20:28:50 +0200

    bump version number to 1.4.9; include release message

--- matroska/KaxVersion.h
+++ matroska/KaxVersion.h
@@ -40,7 +40,7 @@
 
 START_LIBMATROSKA_NAMESPACE
 
-#define LIBMATROSKA_VERSION 0x010408
+#define LIBMATROSKA_VERSION 0x010409
 
 extern const std::string KaxCodeVersion;
 extern const std::string KaxCodeDate;

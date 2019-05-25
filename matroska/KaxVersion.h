e83aa1188ecfc699c4a6accf612e435e39b1fe2c
Moritz Bunkus <moritz@bunkus.org>
2019-05-25 12:16:30 +0200

    bump version number to 1.5.1

--- matroska/KaxVersion.h
+++ matroska/KaxVersion.h
@@ -40,7 +40,7 @@
 
 START_LIBMATROSKA_NAMESPACE
 
-#define LIBMATROSKA_VERSION 0x010500
+#define LIBMATROSKA_VERSION 0x010501
 
 extern const std::string KaxCodeVersion;
 extern const std::string KaxCodeDate;

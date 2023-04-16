bd6e0721dd582501b9f8eacd9b11a420a467cff4
Steve Lhomme <robux4@ycbcr.xyz>
2023-04-16 09:49:16 +0200

    catch the DataBuffer allocation failure
    Using std::nothrow in a header risks breaking when included from a project
    that defines `new` to a different signature.
    
    For example projects using a special version of new on Windows to detect
    memory leaks: https://learn.microsoft.com/en-us/cpp/c-runtime-library/find-memory-leaks-using-the-crt-library

--- matroska/KaxBlock.h
+++ matroska/KaxBlock.h
@@ -68,11 +68,12 @@ class MATROSKA_DLL_API DataBuffer {
     {
       if (bInternalBuffer)
       {
-        myBuffer = new (std::nothrow) binary[mySize];
-        if (!myBuffer)
-          bValidValue = false;
-        else
+        try {
+          myBuffer = new binary[mySize];
           memcpy(myBuffer, aBuffer, mySize);
+        } catch (const std::bad_alloc &) {
+          bValidValue = false;
+        }
       }
       else
         myBuffer = aBuffer;

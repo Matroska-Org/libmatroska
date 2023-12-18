4213b5c884885fc33412f618f922f7950c31d5cb
Steve Lhomme <slhomme@matroska.org>
2023-12-18 08:49:57 +0100

    test9: remove includes that just point to KaxSemantic.h

--- test/tags/test9.cpp
+++ test/tags/test9.cpp
@@ -46,18 +46,11 @@
 #include <matroska/FileKax.h>
 #include <matroska/KaxBlock.h>
 #include <matroska/KaxCluster.h>
-#include <matroska/KaxClusterData.h>
 #include <matroska/KaxCues.h>
-#include <matroska/KaxInfo.h>
 #include <matroska/KaxInfoData.h>
 #include <matroska/KaxSeekHead.h>
 #include <matroska/KaxSegment.h>
-#include <matroska/KaxTags.h>
-#include <matroska/KaxTag.h>
 #include <matroska/KaxTracks.h>
-#include <matroska/KaxTrackEntryData.h>
-#include <matroska/KaxTrackAudio.h>
-#include <matroska/KaxTrackVideo.h>
 #include <matroska/KaxVersion.h>
 
 using namespace std;

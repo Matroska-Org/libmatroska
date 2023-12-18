bb44f43f9603e2a53808426d287d4e2275c348c2
Steve Lhomme <slhomme@matroska.org>
2023-12-18 13:00:09 +0100

    Use a SPDX header for the LGPL 2.1 license
    The copytight date and holder are kept as-is.

--- matroska/KaxDefines.h
+++ matroska/KaxDefines.h
@@ -1,29 +1,5 @@
-/****************************************************************************
-** libmatroska : parse Matroska files, see http://www.matroska.org/
-**
-** <file/class description>
-**
-** Copyright (C) 2002-2010 Steve Lhomme.  All rights reserved.
-**
-** This library is free software; you can redistribute it and/or
-** modify it under the terms of the GNU Lesser General Public
-** License as published by the Free Software Foundation; either
-** version 2.1 of the License, or (at your option) any later version.
-**
-** This library is distributed in the hope that it will be useful,
-** but WITHOUT ANY WARRANTY; without even the implied warranty of
-** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-** Lesser General Public License for more details.
-**
-** You should have received a copy of the GNU Lesser General Public
-** License along with this library; if not, write to the Free Software
-** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
-**
-** See http://www.gnu.org/licenses/lgpl-2.1.html for LGPL licensing information.**
-** Contact license@matroska.org if any conditions of this licensing are
-** not clear to you.
-**
-**********************************************************************/
+// Copyright © 2002-2010 Steve Lhomme.
+// SPDX-License-Identifier: LGPL-2.1-or-later
 
 /*!
   \file

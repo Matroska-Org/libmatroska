c69b33ae97463030e2ef71dfc90929ef2bd445e9
Steve Lhomme <slhomme@matroska.org>
2024-03-03 14:28:01 +0100

    mkvtree: only use libebml/libmatroska API's
    replace the ported API's to the actual API's.

--- test/mkvtree/mkvtree.cpp
+++ test/mkvtree/mkvtree.cpp
@@ -1,116 +1,103 @@
 // Copyright © 2008-2024 Matroska (non-profit organisation).
 // SPDX-License-Identifier: BSD-3-Clause
 
-#include "porting.h"
+#include <ebml/EbmlMaster.h>
+#include <ebml/EbmlUInteger.h>
+#include <ebml/EbmlSInteger.h>
+#include <ebml/EbmlUnicodeString.h>
+#include <ebml/EbmlString.h>
+#include <ebml/EbmlUnicodeString.h>
+#include <ebml/EbmlFloat.h>
+#include <ebml/EbmlDate.h>
+#include <ebml/EbmlVoid.h>
+#include <ebml/EbmlCrc32.h>
+#include <ebml/EbmlStream.h>
+#include <ebml/EbmlHead.h>
+#include <ebml/EbmlDummy.h>
+#include <ebml/StdIOCallback.h>
+
+#include <matroska/KaxSegment.h>
+
+#include <cinttypes>
+#include <cstdio>
+#include <ctime>
 
 using namespace libebml;
 
 static int ShowPos = 0;
 
+#if 0 // TODO allowing find top level elements using an EbmlSemanticContextMaster
 DEFINE_START_SEMANTIC(KaxStream)
 DEFINE_SEMANTIC_ITEM(true, true, libebml::EbmlHead)
 DEFINE_SEMANTIC_ITEM(true, true, libmatroska::KaxSegment)
 DEFINE_END_SEMANTIC(KaxStream)
 
 DEFINE_xxx_CONTEXT(KaxStream,libmatroska::GetKaxGlobal_Context)
+#endif
 
-#define MATROSKA_getContextStream()  EBML_CLASS_SEMCONTEXT(KaxStream)
-
-#ifdef TARGET_WIN
-#include <windows.h>
-void DebugMessage(const tchar_t* Msg,...)
+struct datepack_t
 {
-#if !defined(NDEBUG) || defined(LOGFILE) || defined(LOGTIME)
-	va_list Args;
-	tchar_t Buffer[1024],*s=Buffer;
+    intptr_t Year; // 2001, 2007, etc
+    intptr_t Month; // 1-12
+    intptr_t Day; // 1-31
+    intptr_t Hour; // 24-hour
+    intptr_t Minute; // 0-59
+    intptr_t Second; // 0-59
+    intptr_t WeekDay; // 1 = Sunday, 2 = Monday, etc
+};
 
-	va_start(Args,Msg);
-	vstprintf_s(Buffer,TSIZEOF(Buffer), Msg, Args);
-	va_end(Args);
-	tcscat_s(Buffer,TSIZEOF(Buffer),T("\r\n"));
-#endif
 
-#ifdef LOGTIME
-    {
-        tchar_t timed[1024];
-        SysTickToString(timed,TSIZEOF(timed),GetTimeTick(),1,1,0);
-        stcatprintf_s(timed,TSIZEOF(timed),T(" %s"),s);
-        s = timed;
-    }
-#endif
-
-#if !defined(NDEBUG)
-	OutputDebugString(s);
-#endif
-
-#if defined(LOGFILE)
+static void EndLine(EbmlElement *Element)
 {
-    static FILE* f=NULL;
-    static char s8[1024];
-    size_t i;
-    if (!f)
-        f=fopen("\\corelog.txt","a+b");
-    for (i=0;s[i];++i)
-        s8[i]=(char)s[i];
-    s8[i]=0;
-    fputs(s8,f);
-    fflush(f);
-}
-#endif
-}
-#endif
-
-static void EndLine(ebml_element *Element)
-{
-    if (ShowPos && EBML_ElementPosition(Element)!=INVALID_FILEPOS_T)
-        fprintf(stdout," at %" PRId64 "\r\n",EBML_ElementPosition(Element));
+    if (ShowPos && (Element)->GetElementPosition()!=INVALID_FILEPOS_T)
+        fprintf(stdout," at %" PRId64 "\r\n",(Element)->GetElementPosition());
     else
         fprintf(stdout,"\r\n");
 }
 
-static ebml_element *OutputElement(ebml_element *Element, const ebml_parser_context *Context, stream *Input, unsigned int *Level)
+static EbmlElement *OutputElement(EbmlElement *Element, const EbmlSemanticContext *Context, EbmlStream *Input, unsigned int *Level)
 {
     assert(*Level < 10); // safety check
-    int LevelPrint;
+    unsigned int LevelPrint;
     for (LevelPrint=0;LevelPrint<*Level;++LevelPrint)
         fprintf(stdout,"+ ");
-    fprintf(stdout,"%s: ", EBML_ElementGetClassName(Element));
-    if (Node_IsPartOf(Element,EBML_MASTER_CLASS))
+    fprintf(stdout,"%s: ", EBML_NAME(Element));
+    if (dynamic_cast<const EbmlMaster*>(Element) != nullptr)
     {
         int UpperElement = 0;
-        ebml_element *SubElement,*NewElement;
-        // ebml_parser_context SubContext;
+        EbmlElement *SubElement,*NewElement;
+        // EbmlSemanticContext SubContext;
 
-        if (EBML_ElementDataSize(Element, 1) == INVALID_FILEPOS_T)
+        if (Element->GetSize() == INVALID_FILEPOS_T)
             fprintf(stdout,"(master)");
-        else if (!EBML_ElementIsFiniteSize(Element))
+        else if (!(Element)->IsFiniteSize())
             fprintf(stdout,"(master) [unknown size]");
         else
-            fprintf(stdout,"(master) [%" PRId64 " bytes]",EBML_ElementDataSize(Element, 1));
+            fprintf(stdout,"(master) [%" PRId64 " bytes]",Element->GetSize());
         EndLine(Element);
         // SubContext.UpContext = Context;
-        // SubContext.Context = EBML_ElementContext(Element);
-        // SubContext.EndPosition = EBML_ElementPositionEnd(Element);
+        // SubContext.Context = EBML_CONTEXT(Element);
+        // SubContext.EndPosition = Element->GetEndPosition();
         // SubContext.Profile = Context->Profile;
         // SubElement = EBML_FindNextElement(Input, &SubContext, &UpperElement, 1);
-        SubElement = EBML_FindNextElement(Input, EBML_ElementContext(Element), &UpperElement, 1);
-		while (SubElement != NULL && UpperElement<=0 && (!EBML_ElementIsFiniteSize(Element) || EBML_ElementPosition(SubElement) <= EBML_ElementPositionEnd(Element)))
+        SubElement = Input->FindNextElement(EBML_CONTEXT(Element), UpperElement, UINT64_MAX, true);
+		while (SubElement != NULL && UpperElement<=0 && (!(Element)->IsFiniteSize() || (SubElement)->GetElementPosition() <= Element->GetEndPosition()))
         {
             // a sub element == not higher level and contained inside the current element
             (*Level)++;
-            NewElement = OutputElement(SubElement, &EBML_ElementContext(Element), Input, Level);
-            NodeDelete((node*)SubElement);
+            NewElement = OutputElement(SubElement, &EBML_CONTEXT(Element), Input, Level);
+            delete SubElement;
             if (NewElement)
                 SubElement = NewElement;
             else
             {
-                if (EBML_ElementIsFiniteSize(Element) && Input->I_O().getFilePointer() >= EBML_ElementPositionEnd(Element))
+                if ((Element)->IsFiniteSize() && Input->I_O().getFilePointer() >= Element->GetEndPosition())
                 {
                     SubElement = nullptr;
                     // break;
                 }
                 else
-                    SubElement = EBML_FindNextElement(Input, EBML_ElementContext(Element), &UpperElement, 1);
+                    SubElement = Input->FindNextElement(EBML_CONTEXT(Element), UpperElement, UINT64_MAX, true);
             }
             assert(*Level > 0);
             (*Level)--;
@@ -123,108 +110,119 @@ static ebml_element *OutputElement(ebml_element *Element, const ebml_parser_cont
             }
 			else if (UpperElement != 0 && *Level>0)
             {
-                assert(*Level >= UpperElement-1);
+                assert((int)*Level >= UpperElement-1);
 				*Level -= UpperElement-1;
             }
         }
         return SubElement;
         //EBML_ElementSkipData(Element, Input, Element->Context, NULL, 0);
     }
-    else if (Node_IsPartOf(Element,EBML_VOID_CLASS))
+    else if (dynamic_cast<const EbmlVoid*>(Element) != nullptr)
     {
-        fprintf(stdout,"[%" PRId64 " bytes]",EBML_ElementDataSize(Element, 1));
-        EBML_ElementSkipData(Element, Input, *Context, NULL, 0);
+        fprintf(stdout,"[%" PRId64 " bytes]",Element->GetSize());
+        Element->SkipData(*Input, *Context, nullptr, 0);
         EndLine(Element);
 	}
-    else if (Node_IsPartOf(Element,EBML_CRC_CLASS))
+    else if (dynamic_cast<const EbmlCrc32*>(Element) != nullptr)
     {
         // TODO: handle crc32
-        fprintf(stdout,"[%" PRId64 " bytes]",EBML_ElementDataSize(Element, 1));
-        EBML_ElementSkipData(Element, Input, *Context, NULL, 0);
+        fprintf(stdout,"[%" PRId64 " bytes]",Element->GetSize());
+        Element->SkipData(*Input, *Context, nullptr, 0);
         EndLine(Element);
 	}
-    else if (Node_IsPartOf(Element,EBML_STRING_CLASS) || Node_IsPartOf(Element,EBML_UNISTRING_CLASS))
+    else if (dynamic_cast<const EbmlString*>(Element) != nullptr || dynamic_cast<const EbmlUnicodeString*>(Element) != nullptr)
     {
         //tchar_t UnicodeString[MAXDATA];
-        //EBML_StringRead((ebml_string*)Element,Input,UnicodeString,TSIZEOF(UnicodeString));
-        if (EBML_ElementReadData(Element,Input,NULL,0,SCOPE_ALL_DATA,0)==ERR_NONE)
+        if (Element->ReadData(Input->I_O(),SCOPE_ALL_DATA) != INVALID_FILEPOS_T)
         {
-            tchar_t String[MAXDATA];
-            char cString[MAXDATA];
-            if (Node_IsPartOf(Element,EBML_STRING_CLASS))
-                EBML_StringGet((ebml_string*)Element, String, TSIZEOF(String));
+            if (dynamic_cast<const EbmlString*>(Element) != nullptr)
+                fprintf(stdout,"'%s'",static_cast<const EbmlString *>(Element)->GetValue().c_str());
             else
-                EBML_StringGetUnicode((ebml_unistring*)Element, String, TSIZEOF(String));
-            Node_ToUTF8(Element, cString, sizeof(cString), String);
-            fprintf(stdout,"'%s'",cString);
+                fprintf(stdout,"'%s'",static_cast<const EbmlUnicodeString *>(Element)->GetValueUTF8().c_str());
         } else
             fprintf(stdout,"<error reading>");
         EndLine(Element);
     }
-    else if (Node_IsPartOf(Element,EBML_DATE_CLASS))
+    else if (dynamic_cast<const EbmlDate*>(Element) != nullptr)
     {
-        if (EBML_ElementReadData(Element,Input,NULL,0,SCOPE_ALL_DATA,0)==ERR_NONE)
+        if (Element->ReadData(Input->I_O(),SCOPE_ALL_DATA) != INVALID_FILEPOS_T)
         {
+            auto DateTime = static_cast<const EbmlDate *>(Element)->GetEpochDate();
             datepack_t Date;
-            datetime_t DateTime = EBML_DateTime((ebml_date*)Element);
-            GetDatePacked(DateTime,&Date,1);
+            time_t ot;
+            struct tm *date;
+
+            ot = DateTime;
+            date = localtime(&ot);
+
+            if (date)
+            {
+                Date.Second = date->tm_sec;
+                Date.Minute = date->tm_min;
+                Date.Hour = date->tm_hour;
+                Date.Day = date->tm_mday;
+                Date.Month = date->tm_mon + 1;
+                Date.Year = date->tm_year + 1900;
+                Date.WeekDay = date->tm_wday + 1;
+            }
+
             fprintf(stdout,"%04" PRIdPTR "-%02" PRIdPTR "-%02" PRIdPTR " %02" PRIdPTR ":%02" PRIdPTR ":%02" PRIdPTR " UTC",Date.Year,Date.Month,Date.Day,Date.Hour,Date.Minute,Date.Second);
         }
         else
             fprintf(stdout,"<error reading>");
         EndLine(Element);
     }
-    else if (Node_IsPartOf(Element,EBML_INTEGER_CLASS) || Node_IsPartOf(Element,EBML_SINTEGER_CLASS))
+    else if (dynamic_cast<const EbmlUInteger*>(Element) != nullptr || dynamic_cast<const EbmlSInteger*>(Element) != nullptr)
     {
-        if (EBML_ElementReadData(Element,Input,NULL,0,SCOPE_ALL_DATA,0)==ERR_NONE)
+        if (Element->ReadData(Input->I_O(),SCOPE_ALL_DATA) != INVALID_FILEPOS_T)
         {
-            if (Node_IsPartOf(Element,EBML_SINTEGER_CLASS))
-                fprintf(stdout,"%" PRId64,EBML_IntegerValue((ebml_integer*)Element));
+            if (dynamic_cast<const EbmlSInteger*>(Element) != nullptr)
+                fprintf(stdout,"%" PRId64,static_cast<const EbmlSInteger *>(Element)->GetValue());
             else
-                fprintf(stdout,"%" PRIu64,EBML_IntegerValue((ebml_integer*)Element));
+                fprintf(stdout,"%" PRIu64,static_cast<const EbmlUInteger *>(Element)->GetValue());
         }
         else
             fprintf(stdout,"<error reading>");
         EndLine(Element);
     }
-    else if (Node_IsPartOf(Element,EBML_FLOAT_CLASS))
+    else if (dynamic_cast<const EbmlFloat*>(Element) != nullptr)
     {
-        if (EBML_ElementReadData(Element,Input,NULL,0,SCOPE_ALL_DATA,0)==ERR_NONE)
-            fprintf(stdout,"%f",EBML_FloatValue((ebml_float*)Element));
+        if (Element->ReadData(Input->I_O(),SCOPE_ALL_DATA) != INVALID_FILEPOS_T)
+            fprintf(stdout,"%f",static_cast<const EbmlFloat *>(Element)->GetValue());
         else
             fprintf(stdout,"<error reading>");
         EndLine(Element);
     }
-    else if (EBML_ElementIsDummy(Element))
+    else if (Element->IsDummy())
     {
-        fprintf(stdout,"[%X] [%" PRId64 " bytes]",EBML_ElementClassID(Element),EBML_ElementDataSize(Element, 1));
-        EBML_ElementSkipData(Element, Input, *Context, NULL, 0);
+        fprintf(stdout,"[%X] [%" PRId64 " bytes]",static_cast<const EbmlDummy *>(Element)->GetClassId().GetValue(),Element->GetSize());
+        Element->SkipData(*Input, *Context, nullptr, 0);
         EndLine(Element);
     }
-    else if (Node_IsPartOf(Element,EBML_BINARY_CLASS))
+    else if (dynamic_cast<const EbmlBinary*>(Element) != nullptr)
     {
-        if (EBML_ElementReadData(Element,Input,NULL,0,SCOPE_PARTIAL_DATA,0)==ERR_NONE)
+        if (Element->ReadData(Input->I_O(),SCOPE_PARTIAL_DATA) != INVALID_FILEPOS_T)
         {
-            const uint8_t *Data = EBML_BinaryGetData((ebml_binary*)Element);
-            if (EBML_ElementDataSize(Element, 1) != 0)
+            const uint8_t *Data = static_cast<const EbmlBinary *>(Element)->GetBuffer();
+            if (Element->GetSize() != 0)
             {
                 if (Data==NULL)
-                    fprintf(stdout,"[data too large] (%" PRId64 ")",EBML_ElementDataSize(Element, 1));
-                else if (EBML_ElementDataSize(Element, 1) == 1)
-                    fprintf(stdout,"%02X (%" PRId64 ")",Data[0],EBML_ElementDataSize(Element, 1));
-                else if (EBML_ElementDataSize(Element, 1) == 2)
-                    fprintf(stdout,"%02X %02X (%" PRId64 ")",Data[0],Data[1],EBML_ElementDataSize(Element, 1));
-                else if (EBML_ElementDataSize(Element, 1) == 3)
-                    fprintf(stdout,"%02X %02X %02X (%" PRId64 ")",Data[0],Data[1],Data[2],EBML_ElementDataSize(Element, 1));
-                else if (EBML_ElementDataSize(Element, 1) == 4)
-                    fprintf(stdout,"%02X %02X %02X %02X (%" PRId64 ")",Data[0],Data[1],Data[2],Data[3],EBML_ElementDataSize(Element, 1));
+                    fprintf(stdout,"[data too large] (%" PRId64 ")",Element->GetSize());
+                else if (Element->GetSize() == 1)
+                    fprintf(stdout,"%02X (%" PRId64 ")",Data[0],Element->GetSize());
+                else if (Element->GetSize() == 2)
+                    fprintf(stdout,"%02X %02X (%" PRId64 ")",Data[0],Data[1],Element->GetSize());
+                else if (Element->GetSize() == 3)
+                    fprintf(stdout,"%02X %02X %02X (%" PRId64 ")",Data[0],Data[1],Data[2],Element->GetSize());
+                else if (Element->GetSize() == 4)
+                    fprintf(stdout,"%02X %02X %02X %02X (%" PRId64 ")",Data[0],Data[1],Data[2],Data[3],Element->GetSize());
                 else
-                    fprintf(stdout,"%02X %02X %02X %02X.. (%" PRId64 ")",Data[0],Data[1],Data[2],Data[3],EBML_ElementDataSize(Element, 1));
+                    fprintf(stdout,"%02X %02X %02X %02X.. (%" PRId64 ")",Data[0],Data[1],Data[2],Data[3],Element->GetSize());
             }
         }
         else
             fprintf(stdout,"<error reading>");
-		EBML_ElementSkipData(Element, Input, *Context, NULL, 0);
+		Element->SkipData(*Input, *Context, nullptr, 0);
         EndLine(Element);
     }
     else
@@ -232,54 +230,46 @@ static ebml_element *OutputElement(ebml_element *Element, const ebml_parser_cont
 #ifdef IS_BIG_ENDIAN
         fprintf(stdout,"<unsupported element %x>\r\n",EBML_ElementClassID(Element));
 #else
-        fourcc_t Id = EBML_ElementClassID(Element);
+        auto Id = Element->GetClassId().GetValue();
         fprintf(stdout,"<unsupported element ");
         while (Id & 0xFF)
         {
             fprintf(stdout,"[%x]",Id & 0xFF);
             Id >>= 8;
         }
-        fprintf(stdout,"%X>",EBML_ElementClassID(Element));
+        fprintf(stdout,"%X>",Element->GetClassId().GetValue());
 #endif
-        EBML_ElementSkipData(Element, Input, *Context, NULL, 0);
+        Element->SkipData(*Input, *Context, nullptr, 0);
         EndLine(Element);
     }
     return NULL;
 }
 
-static void OutputEbmlhead(stream *Input)
+static void OutputEbmlhead(EbmlStream *Input)
 {
-    // ebml_element *Element = EBML_ElementCreate(Input,libebml::EbmlHead,0,PROFILE_MATROSKA_ANY,NULL);
-    ebml_element *Element = Input->FindNextID<libebml::EbmlHead>(UINT64_MAX);
-    // ebml_element *Element = EBML_FindNextElement(Input, EBML_CONTEXT(libebml::EbmlHead), &UpperElement, 1);
+    EbmlElement *Element = Input->FindNextID<libebml::EbmlHead>(UINT64_MAX);
     if (Element)
     {
 		unsigned int Level = 0;
-        // EBML_ElementSetInfiniteSize(Element,1);
         OutputElement(Element, NULL, Input, &Level);
-        NodeDelete((node*)Element);
+        delete Element;
     }
 }
 
-static void OutputTree(stream *Input)
+static void OutputTree(EbmlStream *Input)
 {
-    // ebml_element *Element = EBML_ElementCreate(Input,libmatroska::KaxSegment,0,PROFILE_MATROSKA_ANY,NULL);
-    ebml_element *Element = Input->FindNextID<libmatroska::KaxSegment>(UINT64_MAX);
-    // ebml_element *Element = EBML_FindNextElement(Input, EBML_CONTEXT(libmatroska::KaxSegment), &UpperElement, 1);
+    EbmlElement *Element = Input->FindNextID<libmatroska::KaxSegment>(UINT64_MAX);
     if (Element)
     {
 		unsigned int Level = 0;
-        // EBML_ElementSetInfiniteSize(Element,1);
         OutputElement(Element, NULL, Input, &Level);
-        NodeDelete((node*)Element);
+        delete Element;
     }
 }
 
 int main(int argc, const char *argv[])
 {
-    // parsercontext p;
-    stream *Input;
-    tchar_t Path[MAXPATHFULL];
+    EbmlStream *Input;
 
     if ((argc!=2 && argc!=3) || (argc==3 && strcmp(argv[1],"--pos")))
     {
@@ -292,14 +282,8 @@ int main(int argc, const char *argv[])
     if (argc==3)
         ShowPos = 1;
 
-    // Core-C init phase
-    ParserContext_Init(&p,NULL,NULL,NULL);
-    // EBML & Matroska Init
-    MATROSKA_Init(&p);
-
     // open the file to parse
-    Node_FromStr(&p,Path,TSIZEOF(Path),argv[argc-1]);
-    Input = StreamOpen(&p,Path,SFLAG_RDONLY/*|SFLAG_BUFFERED*/);
+    Input = new EbmlStream(*(new StdIOCallback(argv[argc-1], MODE_READ)));
     if (Input == NULL)
         fprintf(stderr, "error: mkvtree cannot open file \"%s\"\r\n",argv[1]);
     else
@@ -307,13 +291,10 @@ int main(int argc, const char *argv[])
         OutputEbmlhead(Input);
         OutputTree(Input);
 
-        StreamClose(Input);
+        Input->I_O().close();
+        delete &(Input->I_O());
+        delete Input;
     }
-
-    // EBML & Matroska ending
-    MATROSKA_Done(&p);
-    // Core-C ending
-    ParserContext_Done(&p);
 
     return 0;
 }
